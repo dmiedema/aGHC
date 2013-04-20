b058383720a9197c6aad7e0501261919627c844d
Daniel Miedema <danielmiedema@me.com>
2013-04-20 00:42:22 -0700

    Unneeded XIB and corresponding class added. for shiggles i guess.

---
+++ DMRepositoryDetailTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  DMRepositoryDetailTableViewCell.h
+//  JSONTesting
+//
+//  Created by Daniel Miedema on 2/26/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DMRepositoryDetailTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) IBOutlet UILabel *largeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *smallLabel;
+
+@end
---
+++ DMRepositoryDetailTableViewCell.m
@@ -0,0 +1,27 @@
+//
+//  DMRepositoryDetailTableViewCell.m
+//  JSONTesting
+//
+//  Created by Daniel Miedema on 2/26/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import "DMRepositoryDetailTableViewCell.h"
+
+@implementation DMRepositoryDetailTableViewCell
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+    
+    // Configure the view for the selected state
+}
+@end
