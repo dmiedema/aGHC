43ee82b6529b836fc7cf510e609c432e4a1cc764
Daniel Miedema <danielmiedema@me.com>
2013-04-27 22:08:50 -0700

    ALL the comment removals.

--- aGHC/DMExploreTableViewController.m
+++ aGHC/DMExploreTableViewController.m
@@ -33,12 +33,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
- 
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
     UIBarButtonItem *homeButton = [[UIBarButtonItem alloc] initWithTitle:@"Home" style:UIBarButtonItemStyleBordered target:self action:@selector(dismissMe:)];
     self.navigationItem.leftBarButtonItem = homeButton;
@@ -115,12 +109,6 @@
     // Configure the cell...
     NSDictionary *currentRepo = [[self repositories] objectAtIndex:[indexPath row]];
     cell.repositoryName.text = [currentRepo objectForKey:@"name"];
-    /*
-    cell.repositoryDetailInfo.text = [NSString stringWithFormat:@"Forks: %@ - Issues: %@ - Watchers: %@",
-                                      [currentRepo objectForKey:@"forks_count"],
-                                      [currentRepo objectForKey:@"open_issues_count"],
-                                      [currentRepo objectForKey:@"watchers"]];
-     */
     
     
     // repo private?
@@ -137,56 +125,11 @@
     return cell;
 }
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 #pragma mark - Table view delegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
     NSDictionary *selectedRepo = [[self repositories] objectAtIndex:[indexPath row]];
     
     DMRepositoryDetailViewController *viewController = [[DMRepositoryDetailViewController alloc] init];
--- aGHC/DMRepositoryDetailViewController.m
+++ aGHC/DMRepositoryDetailViewController.m
@@ -15,30 +15,14 @@
 
 @interface DMRepositoryDetailViewController ()
 @property (nonatomic, strong) NSString *rawHTML;
-
-//@property (nonatomic, strong) UIScrollView *scrollView;
-//
-//@property (nonatomic, strong) IBOutlet UIImageView *imageView;
-//
-//@property (nonatomic, strong) IBOutlet UILabel *repositoryNameLabel;
-//@property (nonatomic, strong) IBOutlet UILabel *descriptionLabel;
-//@property (nonatomic, strong) IBOutlet UILabel *usernameLabel;
-//@property (nonatomic, strong) IBOutlet UILabel *forksLabel;
-//@property (nonatomic, strong) IBOutlet UILabel *watchersLabel;
-//@property (nonatomic, strong) IBOutlet UILabel *issuesLabel;
-//
-//@property (nonatomic,strong) IBOutlet UIWebView *readmeView;
-
 @property (nonatomic, strong) JSNotifier *notifier;
+
 #define stylestring @"<style>*{margin:0;padding:0;}body {font:13.34px helvetica,arial,freesans,clean,sans-serif;color:black;	line-height:1.4em;background-color: #F8F8F8;padding: 0.7em;}p {margin:1em 0;	line-height:1.5em;}table {	font-size:inherit;font:100%margin:1em;}table th{border-bottom:1px solid #bbb;padding:.2em 1em;}table td{border-bottom:1px solid #ddd;padding:.2em 1em;}input[type=text],input[type=password],input[type=image],textarea{font:99% helvetica,arial,freesans,sans-serif;}select,option{padding:0 .25em;}optgroup{margin-top:.5em;}pre,code{font:12px Monaco,'Courier New','DejaVu Sans Mono','Bitstream Vera Sans Mono',monospace;}pre {margin:1em 0;	font-size:12px;	background-color:#eee;border:1px solid #ddd;padding:5px;line-height:1.5em;color:#444;overflow:auto;-webkit-box-shadow:rgba(0,0,0,0.07) 0 1px 2px inset;-webkit-border-radius:3px;-moz-border-radius:3px;border-radius:3px;}pre code {padding:0;font-size:12px;background-color:#eee;border:none;}code {font-size:12px;background-color:#f8f8ff;color:#444;padding:0 .2em;border:1px solid #dedede;}img{border:0;max-width:100%;}abbr{border-bottom:none;}a{color:#4183c4;text-decoration:none;}a:hover{text-decoration:underline;}a code,a:link code,a:visited code{color:#4183c4;}h2,h3{margin:1em 0;}h1,h2,h3,h4,h5,h6{border:0;}h1{font-size:170%;border-top:4px solid #aaa;padding-top:.5em;margin-top:1.5em;}h1:first-child{margin-top:0;padding-top:.25em;border-top:none;}h2{font-size:150%;margin-top:1.5em;border-top:4px solid #e0e0e0;padding-top:.5em;}h3{margin-top:1em;}hr{border:1px solid #ddd;}ul{margin:1em 0 1em 2em;}ol{margin:1em 0 1em 2em;}ul li,ol li{margin-top:.5em;margin-bottom:.5em;}ul ul,ul ol,ol ol,ol ul{margin-top:0;margin-bottom:0;}blockquote{margin:1em 0;border-left:5px solid #ddd;padding-left:.6em;color:#555;}dt{font-weight:bold;margin-left:1em;}dd{margin-left:2em;margin-bottom:1em;}@media screen and (min-width: 768px) {body {width: 748px;margin:10px auto;}}</style>"
 
 
 @end
 
 @implementation DMRepositoryDetailViewController
-
-//TODO: Handle not getting all repo information, load information in async and update UI.
-// might have to set everyhting to reference self and set up a bunch of properties. oh yay.
 
 #define LABEL_WIDTH  280
 #define LABEL_HEIGHT 54
@@ -60,9 +44,7 @@
     [super viewDidLoad];
     [[self navigationController] setTitle:@"Details"];
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(setLabelInformation) name:@"aGHC-RepoDetailsLoaded" object:nil];
-//    [[self scrollView] setFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-//    [[self scrollView] setBackgroundColor:[UIColor blueColor]];
-//    [[self view] addSubview:[self scrollView]];
+
     if (![self repo] && [self repoName] && [self ownerName]) {
         // perform operation to get shit
         NSLog(@"no repo, repoName and ownerName");
@@ -91,202 +73,6 @@
     } else
         [self setLabelInformation];
 	// Do any additional setup after loading the view.
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//    NSLog(@"\n\nInformation to load: %@", [self repo]);
-//    NSDictionary *owner = [[self repo] objectForKey:@"owner"];
-//    // Custom initialization
-//    // create scroll view
-//    float x = 20.0;
-//    float y = 0.0;
-//    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
-//    UIFont *defaultFont = [UIFont fontWithName:@"Avenir" size:20.0];
-//    
-//    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, y, 320, 320)];
-//    [imageView setFrame:CGRectMake(0, y, 320, 320)];
-//    [imageView setImageWithURL:[NSURL URLWithString:[owner objectForKey:@"avatar_url"]] placeholderImage:[UIImage imageNamed:@"sL7cyZ5Oa7-2-1"]];
-//    //    [imageView setImage:[UIImage imageNamed:@"sL7cyZ5Oa7-2-1"]];
-//    // set up dismiss button
-//    UIButton *dismissButton = [[UIButton alloc] init];
-//    [dismissButton setBackgroundColor:[UIColor colorWithRed:200 green:200 blue:200 alpha:0.7]];
-//    //    [dismissButton setImage:[UIImage imageNamed:@"NotifyX"] forState:UIControlStateNormal];
-//    [dismissButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-//    [dismissButton setTitle:@"X" forState:UIControlStateNormal];
-//    [dismissButton setTitle:@":(" forState:UIControlStateHighlighted];
-//    [dismissButton setTag:0];
-//    [dismissButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    [dismissButton setFrame:CGRectMake(10, 10, 44, 44)];
-//    
-//    // currently 320 down after image
-//    y+=320;
-//    NSLog(@"%f", y);
-//    
-//    // set up repository name label
-//    UILabel *repositoryNameLabel = [[UILabel alloc] init];
-//    // set it a glow
-//    [[repositoryNameLabel layer] setShadowColor:[[UIColor blackColor] CGColor]];
-//    [[repositoryNameLabel layer] setShadowRadius:1.0f];
-//    [[repositoryNameLabel layer] setShadowOpacity:1];
-//    [[repositoryNameLabel layer] setShadowOffset:CGSizeZero];
-//    [[repositoryNameLabel layer] setMasksToBounds:NO];
-//    // set it up
-//    [repositoryNameLabel setFont:[UIFont fontWithName:@"Avenir" size:32.0]];
-//    [repositoryNameLabel setAdjustsFontSizeToFitWidth:YES];
-//    [repositoryNameLabel setMinimumScaleFactor:.1];
-//    [repositoryNameLabel setNumberOfLines:1];
-//    [repositoryNameLabel setAdjustsFontSizeToFitWidth:YES];
-//    [repositoryNameLabel setText:[[self repo] objectForKey:@"name"]];
-//    [repositoryNameLabel setBackgroundColor:[UIColor clearColor]];
-//    [repositoryNameLabel setTextColor:[UIColor whiteColor]];
-//    [repositoryNameLabel setFrame:CGRectMake((self.view.frame.size.width / 2)-45, 220, LABEL_WIDTH-75, LABEL_HEIGHT)];
-//    
-//    // set up description label
-//    UILabel *descriptionLabel = [[UILabel alloc] init];
-//    [descriptionLabel setFont:defaultFont];
-//    [descriptionLabel setLineBreakMode:NSLineBreakByWordWrapping];
-//    [descriptionLabel setNumberOfLines:0];
-//    [descriptionLabel setBackgroundColor:[UIColor clearColor]];
-//    [descriptionLabel setTextColor:[UIColor darkGrayColor]];
-//    NSString *descrption = ([[self repo] objectForKey:@"description"] != NULL) ? [[self repo] objectForKey:@"description"] : @"" ;
-//    CGSize constraintSize = CGSizeMake(LABEL_WIDTH, MAXFLOAT);
-//    CGSize labelSize = [descrption sizeWithFont:defaultFont constrainedToSize:constraintSize lineBreakMode:NSLineBreakByWordWrapping];
-//    [descriptionLabel setFrame:CGRectMake(x, y, labelSize.width, labelSize.height)];
-//    [descriptionLabel setText:descrption];
-//    //
-//    y += labelSize.height;
-//    NSLog(@"%f", y);
-//    // set up username label
-//    
-//    UILabel *usernameLabel = [[UILabel alloc] init];
-//    [usernameLabel setFont:defaultFont];
-//    if ([[[self repo] objectForKey:@"fork"] integerValue] == NO)
-//        [usernameLabel setText:[NSString stringWithFormat:@"Created by - %@", [owner objectForKey:@"login"]]];
-//    else [usernameLabel setText:[NSString stringWithFormat:@"Forked by - %@", [owner objectForKey:@"login"]]];
-//    [usernameLabel setBackgroundColor:[UIColor clearColor]];
-//    [usernameLabel setFrame:CGRectMake(x, y, LABEL_WIDTH, LABEL_HEIGHT)];
-//    //
-//    y += LABEL_HEIGHT;
-//    NSLog(@"%f", y);
-//    // set up forks button
-//    UIButton *forksButton = [[UIButton alloc] init];
-//    [forksButton setTitle:@"Fork" forState:UIControlStateNormal];
-//    [forksButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    [forksButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-//    [forksButton setTitleShadowColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-//    // [forksButton setBackgroundImage:[UIColor grayColor] forState:UIControlStateHighlighted];
-//    [forksButton setFrame:CGRectMake(x+10, y, BUTTON_WIDTH, BUTTON_HEIGHT)];
-//    // Set up watch button
-//    UIButton *watchButton = [[UIButton alloc] init];
-//    [watchButton setTitle:@"Watch" forState:UIControlStateNormal];
-//    [watchButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-//    [watchButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    [watchButton setTitleShadowColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-//    [watchButton setFrame:CGRectMake((x+(10*3)) + BUTTON_WIDTH, y, BUTTON_WIDTH, BUTTON_HEIGHT)];
-//    // set up star button
-//    UIButton *starButton = [[UIButton alloc] init];
-//    [starButton setTitle:@"Star" forState:UIControlStateNormal];
-//    [starButton setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-//    [starButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    [starButton setTitleShadowColor:[UIColor lightGrayColor] forState:UIControlStateHighlighted];
-//    [starButton setFrame:CGRectMake((x+(10*5)) + (BUTTON_WIDTH * 2), y, BUTTON_WIDTH, BUTTON_HEIGHT)];
-//    //
-//    y += BUTTON_HEIGHT;
-//    NSLog(@"%f", y);
-//    // set up labels below buttons
-//    UILabel *forksLabel = [[UILabel alloc] init];
-//    [forksLabel setFont:defaultFont];
-//    [forksLabel setText:[NSString stringWithFormat:@"Forks - %@",[[self repo] objectForKey:@"forks_count"]]];
-//    [forksLabel setBackgroundColor:[UIColor clearColor]];
-//    [forksLabel setFrame:CGRectMake(x, y, LABEL_WIDTH/2, LABEL_HEIGHT)];
-//    // set up watchers label below buttons
-//    UILabel *watchersLabel = [[UILabel alloc] init];
-//    [watchersLabel setFont:defaultFont];
-//    [watchersLabel setText:[NSString stringWithFormat:@"Watchers - %@", [[self repo] objectForKey:@"watchers_count"]]];
-//    [watchersLabel setBackgroundColor:[UIColor clearColor]];
-//    [watchersLabel setFrame:CGRectMake((LABEL_WIDTH+x) / 2, y, LABEL_WIDTH/2, LABEL_HEIGHT)];
-//    //
-//    y += LABEL_HEIGHT;
-//    NSLog(@"%f", y);
-//    // issues label
-//    UILabel *issuesLabel = [[UILabel alloc] init];
-//    [issuesLabel setFont:defaultFont];
-//    [issuesLabel setText:[NSString stringWithFormat:@"Open Issues - %@",[[self repo] objectForKey:@"open_issues_count"]]];
-//    [issuesLabel setBackgroundColor:[UIColor clearColor]];
-//    [issuesLabel setFrame:CGRectMake(x, y, LABEL_WIDTH, LABEL_HEIGHT)];
-//    //
-//    y += LABEL_HEIGHT;
-//    NSLog(@"%f", y);
-//    // explore code button
-//    UIButton *exploreCode = [[UIButton alloc] init];
-//    [exploreCode setTitle:@"Explore Code" forState:UIControlStateNormal];
-//    [exploreCode setTitleColor:[UIColor darkGrayColor] forState:UIControlStateNormal];
-//    [exploreCode addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
-//    [exploreCode setFrame:CGRectMake(x, y, LABEL_WIDTH, BUTTON_HEIGHT)];
-//    y += BUTTON_HEIGHT;
-//    NSLog(@"%f", y);
-//    
-//    // create readme markdown view
-//    UIWebView *readmeView = [[UIWebView alloc] init];
-//    // Accept Header
-//    // application/vnd.github.v3.html+json
-//    // on https://api.github.com/repos/dmiedema/dropboxcollection/readme
-//    // or kGitHubApiURL /repos/:user/:repo/readme
-//    // and render output to webview
-//    NSString *requestString;
-//    NSString *token     = [[NSUserDefaults standardUserDefaults] objectForKey:kAccessToken];
-//    NSString *tokenType = [[NSUserDefaults standardUserDefaults] objectForKey:kTokenType];
-//    NSString *login     = [owner objectForKey:@"login"];
-//    NSString *reponame  = [[self repo] objectForKey:@"name"];
-//    
-//    if (token || tokenType == NULL)
-//        requestString = [NSString stringWithFormat:@"%@repos/%@/%@/readme", kGitHubApiURL, login, reponame];
-//    else
-//        requestString = [NSString stringWithFormat:@"%@repos/%@/%@/readme?%@=%@&%@=%@", kGitHubApiURL, login, reponame, kAccessToken, token, kTokenType, tokenType];
-//    
-//    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:requestString]];
-//
-//    NSLog(@"Request - %@", request);
-//    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-//
-//    AFJSONRequestOperation *operation = [AFJSONRequestOperation JSONRequestOperationWithRequest:request success:^(NSURLRequest *request, NSHTTPURLResponse *response, id JSON) {
-//        NSLog(@"Response -- %@", JSON);
-//        NSLog(@"Content - %@", [JSON objectForKey:@"content"]);
-//        NSLog(@"Decoded - %@", [NSString stringFromBase64String:[JSON objectForKey:@"content"]]);
-//        [self setRawHTML:[NSString stringWithFormat:@"%@%@", stylestring, [NSString stringFromBase64String:[JSON objectForKey:@"content"]]]];
-//        
-//        NSLog(@"HTML - %@", [MMMarkdown HTMLStringWithMarkdown:[NSString stringFromBase64String:[JSON objectForKey:@"content"]] error:nil]);
-//        
-//        [readmeView loadHTMLString:[MMMarkdown HTMLStringWithMarkdown:[self rawHTML] error:nil] baseURL:nil];
-//    } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error, id JSON) {
-//        NSLog(@"HTML -- %@", JSON);
-//        NSLog(@"Error - %@", error);
-//        NSLog(@"Error loading README");
-//    }];
-//    [operation start];
-//    [readmeView setAutoresizingMask:UIViewAutoresizingFlexibleBottomMargin];
-//    
-//    [readmeView setFrame:CGRectMake(0, y, self.view.frame.size.width, [[readmeView stringByEvaluatingJavaScriptFromString:@"document.body.scrollHeight"] floatValue])];
-//    [readmeView sizeToFit];
-//    y += readmeView.frame.size.height;
-//    
-//    // add subviews to scroll view. And there are a lot of them holy fuck.
-//    [scrollView addSubview:imageView];
-//    [scrollView addSubview:repositoryNameLabel];
-//    [scrollView addSubview:descriptionLabel];
-//    [scrollView addSubview:usernameLabel];
-//    [scrollView addSubview:forksButton];
-//    [scrollView addSubview:watchButton];
-//    [scrollView addSubview:starButton];
-//    [scrollView addSubview:forksLabel];
-//    [scrollView addSubview:watchersLabel];
-//    [scrollView addSubview:issuesLabel];
-//    [scrollView addSubview:exploreCode];
-//    [scrollView addSubview:dismissButton];
-//    [scrollView addSubview:readmeView];
-//    
-//    // add the scroll view into the view
-//    [scrollView setContentSize:CGSizeMake(self.view.frame.size.width, y)];
-//    [scrollView setBackgroundColor:[UIColor whiteColor]];
-//    [[self view] addSubview:scrollView];
 }
 
 - (void)didReceiveMemoryWarning
@@ -313,6 +99,7 @@
     [imageView setImageWithURL:[NSURL URLWithString:[owner objectForKey:@"avatar_url"]] placeholderImage:[UIImage imageNamed:@"sL7cyZ5Oa7-2-1"]];
     //    [imageView setImage:[UIImage imageNamed:@"sL7cyZ5Oa7-2-1"]];
     // set up dismiss button
+    /* // Not needed anymore, showing the view correctly.
     UIButton *dismissButton = [[UIButton alloc] init];
     [dismissButton setBackgroundColor:[UIColor colorWithRed:200 green:200 blue:200 alpha:0.7]];
     //    [dismissButton setImage:[UIImage imageNamed:@"NotifyX"] forState:UIControlStateNormal];
@@ -322,7 +109,7 @@
     [dismissButton setTag:0];
     [dismissButton addTarget:self action:@selector(buttonPressed:) forControlEvents:UIControlEventTouchUpInside];
     [dismissButton setFrame:CGRectMake(10, 10, 44, 44)];
-    
+    */
     // currently 320 down after image
     y+=320;
     NSLog(@"%f", y);
