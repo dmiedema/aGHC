ca70a9134a537348e59ce31cf6e79eeffc7451fd
Daniel Miedema <danielmiedema@me.com>
2013-05-03 15:38:28 -0700

    Blank aboutViewController. Need to fill this one out.

---
+++ aGHC/DMAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  DMAboutViewController.h
+//  aGHC
+//
+//  Created by Daniel Miedema on 5/3/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DMAboutViewController : UIViewController
+
+@end
---
+++ aGHC/DMAboutViewController.m
@@ -0,0 +1,38 @@
+//
+//  DMAboutViewController.m
+//  aGHC
+//
+//  Created by Daniel Miedema on 5/3/13.
+//  Copyright (c) 2013 Daniel Miedema. All rights reserved.
+//
+
+#import "DMAboutViewController.h"
+
+@interface DMAboutViewController ()
+
+@end
+
+@implementation DMAboutViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
